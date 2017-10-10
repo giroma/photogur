5cccd49b708a3ffa4d035f9512172c2da5c934ea
Argir Argirov <dj.giro@gmail.com>
2017-10-10 16:00:56 -0400

    form created

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   get 'pictures' => 'pictures#index'
+
+  get 'pictures/new' => 'pictures#new'
+  post 'pictures' => 'pictures#create'
+
   get 'pictures/:id' => 'pictures#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
