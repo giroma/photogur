a95ecbff52727d1b71b2d3f7141c4d02bdeb4112
Argir Argirov <dj.giro@gmail.com>
2017-10-10 17:15:03 -0400

    editing pictures added

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,8 @@ Rails.application.routes.draw do
   post 'pictures' => 'pictures#create'
 
   get 'pictures/:id' => 'pictures#show'
+
+  get 'pictures/:id/edit' => 'pictures#edit'
+  patch 'pictures/:id' => 'pictures#update'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
