d7b50502cc973e4e213b43b3433c7f7e45494076
Argir Argirov <dj.giro@gmail.com>
2017-10-16 16:43:06 -0400

    validations added

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,11 +1,16 @@
 class Picture < ApplicationRecord
 
+  validates :artist, :title, :url, presence: true
+  validates :title, length: {minimum: 3, maximum: 20}
+  validates :url, uniqueness: true
+  validates :url, format: { with: /https?:\/\/[\S]+/, message: "only http"}
+
   def self.newest_first
-    Picture.order("created_at ASC")
+    Picture.order("created_at DESC")
   end
 
   def self.most_recent_five
-    Picture.newest_first.limit(5)
+    Picture.newest_first.limit(7)
   end
 
   def self.created_before(time)
