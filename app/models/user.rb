5dba5e5600cf24df1c78b02c1fedef11ab3054ec
Argir Argirov <dj.giro@gmail.com>
2017-10-19 14:55:03 -0400

    created new user view and request handling

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  validates :email, presence: true
+  validates :password, length: {minimum: 3, maximum: 20, message: "min 3   max 20 characters"}
+  validates :email, uniqueness: true
 end
